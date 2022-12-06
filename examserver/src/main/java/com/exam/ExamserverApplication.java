da719a62cbc04cd47f17934f6fdb5f9cf8452cd4
divya.marshettiwar <divya.marshettiwar@host.docker.internal>
2022-12-06 16:52:54 +0530

    committing the user controller

--- examserver/src/main/java/com/exam/ExamserverApplication.java
+++ examserver/src/main/java/com/exam/ExamserverApplication.java
@@ -26,6 +26,8 @@ public class ExamserverApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Starting code...");
+		
+		/*
 		User user = new User();
 		user.setFirstName("Divya");
 		user.setLastName("M");
@@ -47,6 +49,7 @@ public class ExamserverApplication implements CommandLineRunner {
 		
 		User user1 = this.userService.createUser(user, userRoleSet);
 		System.out.println(user1.getUsername());
+		*/
 	}
 
 }
--- examserver/src/main/java/com/exam/service/UserService.java
+++ examserver/src/main/java/com/exam/service/UserService.java
@@ -7,5 +7,12 @@ import com.exam.model.UserRole;
 
 public interface UserService {
 
+	// for creating new user
 	public User createUser(User user, Set<UserRole> userRoles);
+	
+	// for getting the existing username
+	public User getUser(String username);
+	
+	// to delete user
+	public void deleteUser(Long userId);
 }
