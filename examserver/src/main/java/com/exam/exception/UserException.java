71db74de6d82a15de9b86a5cb782e6ef6de951e4
divya.marshettiwar <divya.marshettiwar@host.docker.internal>
2022-12-06 13:40:27 +0530

    creating the repo, service, serviceImpl and adding the user as Admin and store in DB

---
+++ examserver/src/main/java/com/exam/exception/UserException.java
@@ -0,0 +1,8 @@
+package com.exam.exception;
+
+public class UserException extends RuntimeException {
+
+	public UserException(String msg) {
+		super(msg);
+	}
+}
---
+++ examserver/src/main/java/com/exam/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.exam.service.impl;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exam.exception.UserException;
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.repo.RoleRepository;
+import com.exam.repo.UserRepository;
+import com.exam.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+
+	// creating user
+	@Override
+	public User createUser(User user, Set<UserRole> userRoles){
+		User local = this.userRepository.findByUsername(user.getUsername());
+		if(local != null) {
+			throw new UserException("User is already present !!");
+		}else {
+			// to create user 
+			for(UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			user.getUserRoles().addAll(userRoles);
+			local = this.userRepository.save(user);
+		}
+		return local;
+	}
+
+}
